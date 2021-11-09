8c703d3d71d50d6691a614d57d0ac9d11dcf64e0
khodchenko <khoch3nk0@gmail.com>
2021-11-09 12:46:21 +0100

    saved instance

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.geoquiz
 
+
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,16 +21,22 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
+    private val TAG = "QuizActivity"
+    private val KEY_INDEX = "index"
 
     var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle) called")
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
-        var nextButton = findViewById<Button>(R.id.next_button)
 
+        var nextButton = findViewById<ImageButton>(R.id.next_button)
 
 
         var questionTextView = findViewById<TextView>(R.id.question_name)
@@ -52,6 +62,38 @@ class MainActivity : AppCompatActivity() {
         //TODO
 
         }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG, "onStart: called")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume: called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: called")
+    }
+
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, currentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: called")
+    }
 
     fun checkAnswer(userPressedTrue:Boolean){
         val answerIsTrue: Boolean = questionBank.get(currentIndex).getAnswerTrue()
